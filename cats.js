3ba1d0eb8f9cfaaa0c016d912d4f0c4891dcba1a
Alex Jenkins <kajenkins100@gmail.com>
2024-10-23 18:09:04 -0400

    Add files via upload

--- cats.js
+++ cats.js
@@ -1,41 +1,35 @@
-const url = `https://api.thecatapi.com/v1/images/search?limit=5`; // Load 5 images
-const api_key = "MM4ElURC9w1QaqI5S8X1PJOQ0xsHFr8h74ZAkz1YcYSUdPzeINbe5lo8nubku4ZR";
+const desiredCount = 5; // Set the number of cat images to display
+const url = `https://api.thecatapi.com/v1/images/search?limit=10`; // Fetch a larger number to account for potential API issues
 
-fetch(url, {
-  headers: {
-    'x-api-key': api_key
-  }
-})
-.then((response) => response.json())
-.then((data) => {
-  let imagesData = data;
-  imagesData.map(function(imageData) {
-    
-    let image = document.createElement('img');
-    image.src = `${imageData.url}`;
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        let imagesData = data.slice(0, desiredCount); // Limit to the desired count
         
-    let infoBox = document.createElement('div');
-    infoBox.classList.add('info-box');
-    
-    let header = document.createElement('h3');
-    header.innerText = 'Cat Image Info';
-    
-    let paragraph = document.createElement('p');
-    paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-    
-    infoBox.appendChild(header);
-    infoBox.appendChild(paragraph);
+        imagesData.forEach(function(imageData) {
+            let image = document.createElement('img');
+            image.src = `${imageData.url}`;
+            
+            let infoBox = document.createElement('div');
+            infoBox.classList.add('info-box');
+            
+            let header = document.createElement('h3');
+            header.innerText = 'Cat Image Info';
+            
+            let paragraph = document.createElement('p');
+            paragraph.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+            
+            infoBox.appendChild(header);
+            infoBox.appendChild(paragraph);
 
-    let gridCell = document.createElement('div');
-    gridCell.classList.add('col');
-    gridCell.classList.add('col-lg');
-    gridCell.appendChild(image);
-    gridCell.appendChild(infoBox);
-      
-    document.getElementById('grid').appendChild(gridCell);
-    
-  });
-})
-.catch(function(error) {
-  console.log(error);
-});
+            let gridCell = document.createElement('div');
+            gridCell.classList.add('gallery-item'); // Updated to match CSS class
+            gridCell.appendChild(image);
+            gridCell.appendChild(infoBox);
+            
+            document.getElementById('grid').appendChild(gridCell);
+        });
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
